66d8b0874754d2f8889487d48f9e3d2a8c48c0cd
marihachi <marihachi0620@gmail.com>
2024-01-26 21:23:49 +0900

    improve timeline

--- src/views/Timeline.tsx
+++ src/views/Timeline.tsx
@@ -12,7 +12,7 @@ type Props = {
 };
 
 const Timeline: FC<Props> = (props) => {
-  const [notes, setNotes] = useState<string[]>([]);
+  const [notes, setNotes] = useState<{ id: string, text: string, user: { name: string, username: string } }[]>([]);
 
   useEffect(() => {
     let cancellationToken = { isCancel: false };
@@ -25,8 +25,8 @@ const Timeline: FC<Props> = (props) => {
             props.account.host,
             'notes/hybrid-timeline',
             props.account.accessToken,
-            {});
-          setNotes(notes.map((x: any) => x.text));
+            { limit: 20 });
+          setNotes(notes);
         } catch {
           console.error('failed to fetch timeline.');
         }
@@ -50,7 +50,10 @@ const Timeline: FC<Props> = (props) => {
       <ul>
         {
           notes.map(note =>
-            <li>{ note }</li>
+            <li key={ note.id } style={{ marginBottom: "0.5rem" }}>
+              <div style={{ color: '#AAA' }}>{ note.user.name } @{ note.user.username }</div>
+              <div>{ note.text }</div>
+            </li>
           )
         }
       </ul>
