18efffd1c797c21ca465a8fab52ee471e1f10a11
marihachi <marihachi0620@gmail.com>
2024-01-26 20:54:36 +0900

    timeline polling

--- src/views/Timeline.tsx
+++ src/views/Timeline.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import type { FC } from 'react';
 import PostForm from './PostForm.js';
+import { api } from '../models/misskey.js';
+import { sleep } from '../models/util.js';
 
 // import './Timeline.css';
 
@@ -11,11 +13,30 @@ type Props = {
 
 const Timeline: FC<Props> = (props) => {
   const [notes, setNotes] = useState<string[]>([]);
+
   useEffect(() => {
-    console.log('start timeline');
-    //setNotes(['aaa', 'bbb']);
+    let cancellationToken = { isCancel: false };
+    (async () => {
+      if (props.account == null) return;
+      //console.log('start timeline');
+      while (!cancellationToken.isCancel) {
+        try {
+          const notes = await api(
+            props.account.host,
+            'notes/hybrid-timeline',
+            props.account.accessToken,
+            {});
+          setNotes(notes.map((x: any) => x.text));
+        } catch {
+          console.error('failed to fetch timeline.');
+        }
+        await sleep(2000);
+      }
+      //console.log('timeline stopped');
+    })();
     return () => {
-      console.log('dispose timeline');
+      //console.log('stop timeline request');
+      cancellationToken.isCancel = true;
     };
   }, []);
 
