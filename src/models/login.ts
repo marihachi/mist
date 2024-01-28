4cf4eeb780e6241c75e068676f09f8cc922135ca
massongit <suzukimasaya428@gmail.com>
2024-01-28 17:37:07 +0900

    Convert URL in login-host to host name

--- src/models/login.ts
+++ src/models/login.ts
@@ -2,6 +2,8 @@ import { addCredential } from './credential.js';
 import { authorize } from './misskey.js';
 
 export async function tryLogin(mode: string, host: string, cancellationToken: { isCancel: boolean }) {
+  host = host.replace(/^https?:\/\//, '').replace(/\/$/, '')
+
   // start cancellation timer (5 minutes)
   const timer = setTimeout(() => {
     cancellationToken.isCancel = true;
