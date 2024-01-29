a6e23ea8549f87470cbe51bcad8cfecdd70aa6b2
marihachi <marihachi0620@gmail.com>
2024-01-29 16:15:08 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './views/App.js';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-	plugins: [
-		react(),
-		viteSingleFile(),
-	],
-	build: {
-		reportCompressedSize: false,
-	},
+  plugins: [
+    react(),
+    viteSingleFile(),
+  ],
+  build: {
+    reportCompressedSize: false,
+  },
 });
