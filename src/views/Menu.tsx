575d0ba0553b7ce01f104093935fb111d0ab9be3
marihachi <marihachi0620@gmail.com>
2024-01-30 11:56:36 +0900

    menu style

--- src/views/Menu.tsx
+++ src/views/Menu.tsx
@@ -8,34 +8,34 @@ type Props = {
 const Menu: FC<Props> = (props) => {
   return (
     <>
-      <div>
+      <div className='menu'>
         {
           props.pageSet.indexOf('login') != -1 &&
-          <div className='page-button-login'>
+          <div className='menu-button'>
             ログイン
           </div>
         }
         {
           props.pageSet.indexOf('timeline1') != -1 &&
-          <div className='page-button-htl'>
+          <div className='menu-button'>
             ホーム
           </div>
         }
         {
           props.pageSet.indexOf('timeline2') != -1 &&
-          <div className='page-button-ltl'>
+          <div className='menu-button'>
             ローカル
           </div>
         }
         {
           props.pageSet.indexOf('timeline3') != -1 &&
-          <div className='page-button-stl'>
+          <div className='menu-button'>
             ソーシャル
           </div>
         }
         {
           props.pageSet.indexOf('setting') != -1 &&
-          <div className='page-button-setting'>
+          <div className='menu-button'>
             設定
           </div>
         }
